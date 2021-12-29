12e77e9c885e600a91d0bbf2db26917da2bb3085
Lasan Rashmika <lasanrash@gmail.com>
2021-12-30 00:07:23 +0530

    Static Block in Property File

--- src/main/java/utils/ReadPropertyFile.java
+++ src/main/java/utils/ReadPropertyFile.java
@@ -13,13 +13,32 @@ public final class ReadPropertyFile {
     private ReadPropertyFile(){
     }
 
+    private static Properties properties = new Properties();
+
+    //Static will be the first thing to run when the class is loaded.
+    //Execution is fast when there are a lot of test cases. Each time it won't go and check the config.property
+    //It loads one time in the memory and refer from it.
+    static {
+
+        FileInputStream fs = null;
+        try {
+            fs = new FileInputStream(FrameworkConstants.getConfigFilePath());
+            properties.load(fs);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public static String getValue(String key) throws Exception {
 
         String value="";
-        Properties properties = new Properties();
-        FileInputStream fs = new FileInputStream(FrameworkConstants.getConfigFilePath());
-        properties.load(fs);
         value = properties.getProperty(key);
+
         if(Objects.isNull(value)){
 
             throw new Exception("Property name "+ key+ " is not found. Please check the config.properties");
