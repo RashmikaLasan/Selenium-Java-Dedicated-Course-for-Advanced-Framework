75d485b5d36dd466f248e5aaf8d98afbe88089aa
Lasan Rashmika <lasanrash@gmail.com>
2022-01-10 23:38:19 +0530

    Explicit Wait Factory Lasan2

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,5 +1,3 @@
 
 7
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
-Y
-)src/main/java/utils/ReadPropertyFile.java,8\8\886ad19147893558432c25ea51cb67531a7e6b8b
--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -2,66 +2,28 @@ package pages;
 
 import constants.FrameworkConstants;
 import driver.DriverManager;
+import enums.WaitStrategy;
+import factories.ExplicitWaitFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
 
-    protected void sendKeys(By by, String value, String waitStrategy){
+    protected void sendKeys(By by, String value, WaitStrategy waitStrategy){
 
-        if(waitStrategy.equalsIgnoreCase("clickable")){
-
-            waitForElementToBeClickable(by);
-        }
-
-        else if (waitStrategy.equalsIgnoreCase("present")){
-
-            waitForElementToBePresent(by);
-        }
-
-        DriverManager.getDriver().findElement(by).sendKeys(value);
+        ExplicitWaitFactory.performExplicitWaitFactory(by, waitStrategy).sendKeys(value);
     }
 
-    protected void click(By by,String waitStrategy){
+    protected void click(By by, WaitStrategy waitStrategy){
 
-        if(waitStrategy.equalsIgnoreCase("clickable")){
-
-            waitForElementToBeClickable(by);
-        }
-
-        else if (waitStrategy.equalsIgnoreCase("present")){
-
-            waitForElementToBePresent(by);
-        }
-
-        DriverManager.getDriver().findElement(by).click();
+        ExplicitWaitFactory.performExplicitWaitFactory(by, waitStrategy).click();
     }
 
-    protected String getText(By by,String waitStrategy){
+    protected String getText(By by, WaitStrategy waitStrategy){
 
-        if(waitStrategy.equalsIgnoreCase("clickable")){
-
-            waitForElementToBeClickable(by);
-        }
-
-        else if (waitStrategy.equalsIgnoreCase("present")){
-
-            waitForElementToBePresent(by);
-        }
-
+        ExplicitWaitFactory.performExplicitWaitFactory(by, waitStrategy);
         return DriverManager.getDriver().findElement(by).getText();
     }
 
-    private void waitForElementToBeClickable(By by){
-
-        WebDriverWait wait = new WebDriverWait(DriverManager.getDriver(), FrameworkConstants.getWaitTimeInSeconds());
-        wait.until(ExpectedConditions.elementToBeClickable(by));
-    }
-
-    private void waitForElementToBePresent(By by){
-
-        WebDriverWait wait = new WebDriverWait(DriverManager.getDriver(),FrameworkConstants.getWaitTimeInSeconds());
-        wait.until(ExpectedConditions.presenceOfElementLocated(by));
-    }
 }
